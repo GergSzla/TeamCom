d1a45055f0e456fb337930c8aaab38f6ad697442
Gergo Szilagyi <gergo.szla@gmail.com>
2020-10-14 20:29:52 +0100

    -basic popup option added

--- app/src/main/java/ie/wit/teamcom/activities/ChannelsListActivity.kt
+++ app/src/main/java/ie/wit/teamcom/activities/ChannelsListActivity.kt
@@ -1,20 +1,22 @@
 package ie.wit.teamcom.activities
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
-import com.google.firebase.storage.FirebaseStorage
 import ie.wit.teamcom.R
 import ie.wit.teamcom.main.MainApp
 import ie.wit.teamcom.models.Account
 import kotlinx.android.synthetic.main.activity_channels_list.*
 import org.jetbrains.anko.AnkoLogger
 
-class ChannelsListActivity : AppCompatActivity(), AnkoLogger {
+class ChannelsListActivity : AppCompatActivity(), AnkoLogger{
 
     var user = Account()
     lateinit var eventListener : ValueEventListener
@@ -29,7 +31,33 @@ class ChannelsListActivity : AppCompatActivity(), AnkoLogger {
 
         getUser()
 
+        val builder: AlertDialog.Builder? = this.let {
+            AlertDialog.Builder(it)
+        }
+
+        builder?.setTitle("Add New Channel")?.setItems(arrayOf("Create", "Join")
+        ) { _, which ->
+            when (which) {
+                0 -> {
+                    Toast.makeText(this, "Create Selected", Toast.LENGTH_SHORT).show()
+                }
+                1 -> {
+                    Toast.makeText(this, "Join Selected", Toast.LENGTH_SHORT).show()
+
+                }
+            }
+        }
+
+
+        val dialog: AlertDialog? = builder?.create()
+
+
+        btnAddNew.setOnClickListener {
+            dialog?.show()
+        }
     }
+
+
 
     private fun getUser(){
         val uid = FirebaseAuth.getInstance().currentUser!!.uid
@@ -38,7 +66,6 @@ class ChannelsListActivity : AppCompatActivity(), AnkoLogger {
         eventListener = object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
                 user = dataSnapshot.getValue(Account::class.java)!!
-                textView.setText(user.firstName)
 
                 uidRef.removeEventListener(this)
 
@@ -50,4 +77,6 @@ class ChannelsListActivity : AppCompatActivity(), AnkoLogger {
         uidRef.addListenerForSingleValueEvent(eventListener)
 
     }
+
 }
+
